cd0035aceaa502498c82800f01ca88f19358261f
Caleb Kiage <caleb.kiage@gmail.com>
2021-12-01 19:14:22 +0300

    Add program entry point for CLI

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Graph.Cli
+{
+    class Program {
+        static async Task<int> Main(string[] args) {
+            var rootCommand = new RootCommand(); // TODO: Replace with root command from generated client
+            rootCommand.Description = "Microsoft Graph CLI";
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
