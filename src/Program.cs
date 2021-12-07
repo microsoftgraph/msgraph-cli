f881cdebc732b9be1e0a94cdd52f5dc842e09dd8
Caleb Kiage <caleb.kiage@gmail.com>
2021-12-07 16:05:04 +0300

    Initialize commands from generated code.

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,27 @@
-﻿using System.CommandLine;
+﻿using ApiSdk;
+using Azure.Identity;
+using Microsoft.Graph.Cli.Utils;
+using Microsoft.Kiota.Authentication.Azure;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using System.CommandLine;
 using System.Threading.Tasks;
 
 namespace Microsoft.Graph.Cli
 {
     class Program {
         static async Task<int> Main(string[] args) {
-            var rootCommand = new RootCommand(); // TODO: Replace with root command from generated client
+            DeviceCodeCredentialOptions credOptions = new()
+            {
+                ClientId = Constants.ClientId,
+                TenantId = Constants.TenantId
+            };
+
+            var credential = new DeviceCodeCredential(credOptions);
+            var authProvider = new AzureIdentityAuthenticationProvider(credential);
+            var core = new HttpClientRequestAdapter(authProvider);
+            var client = new GraphClient(core);
+
+            var rootCommand = client.BuildCommand();
             rootCommand.Description = "Microsoft Graph CLI";
             return await rootCommand.InvokeAsync(args);
         }
