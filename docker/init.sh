4a6fa1fa71998bb44705326ac7a0d61e9c969716
Caleb Kiage <caleb.kiage@gmail.com>
2022-03-24 18:05:47 +0300

    Add check for IPC_LOCK capability in init script

--- docker/init.sh
+++ docker/init.sh
@@ -5,6 +5,14 @@ set -e
 script="$0"
 DIR="$(dirname $script)"
 
+capabilities=$(capsh --print | grep -e "Current: .*ipc_lock" | sed s/\n// | sed s/\ //)
+ipc_error="IPC_LOCK capability is not enabled. If you are running a docker container, add the capability using the '--cap-add' option."
+
+if [ -z "$capabilities" ]; then
+    echo "$ipc_error"
+    exit 2
+fi
+
 if ! pgrep -x "dbus-daemon" > /dev/null
 then
     export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --session --fork --print-address)
