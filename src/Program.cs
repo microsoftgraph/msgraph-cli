921923095a9cddfa4327b528fe1bf68f8c50103e
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-04-20 06:30:04 +0300

    Fix base directory

--- src/Program.cs
+++ src/Program.cs
@@ -115,7 +115,7 @@ namespace Microsoft.Graph.Cli
         
         static void ConfigureAppConfiguration(IConfigurationBuilder builder) {
             builder.Sources.Clear();
-            builder.AddJsonFile(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "app-settings.json"), optional: true);
+            builder.AddJsonFile(Path.Combine(System.AppContext.BaseDirectory, "app-settings.json"), optional: true);
             var pathUtil = new PathUtility();
             var authCache = new AuthenticationCacheUtility(pathUtil);
             var dataDir = pathUtil.GetApplicationDataDirectory();
