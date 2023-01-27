6740c8d16d2009e775df9e74b64a1dc2ce05b8f1
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-01-27 17:29:42 +0300

    Move zip name computation to powershell module

--- .azure-pipelines/powershell/BuildTools.psm1
+++ .azure-pipelines/powershell/BuildTools.psm1
@@ -1,11 +1,37 @@
-function Zip-Build-Output {
+function Get-Version {
+    param(
+        [string]
+        $BranchOrTagName = "latest"
+    )
+    # Get the version
+    $version = "$BranchOrTagName"
+    $version = $version.TrimStart('refs/tags/v')
+    # Return the version
+    return $version
+}
+
+function Get-ZipName {
+    param(
+        [string]
+        $FileNameTemplate = "msgraph-cli-{0}-{1}.zip",
+        [string]
+        $BranchOrTagName = "latest",
+        [string]
+        $RuntimeIdentifier = "unknown"
+    )
+    $version = Get-Version $BranchOrTagName 
+    return "$FileNameTemplate" -f "$RuntimeIdentifier","$version"
+}
+
+function Zip-BuildOutput {
     param(
         [Parameter(Mandatory)]
         [string]
-        $FileName,
-        [Parameter(Mandatory)]
-        [string]
         $OutputDir,
+        [string]
+        $FileNameTemplate = "msgraph-cli-{0}-{1}.zip",
+        [string]
+        $BranchOrTagName = "latest",
         [string]
         $RuntimeIdentifier = "unknown"
     )
@@ -15,6 +41,7 @@ function Zip-Build-Output {
     }
 
     $outputLocation = "$OutputDir-$RuntimeIdentifier"
+    $zipName = Get-ZipName -FileNameTemplate $FileNameTemplate -BranchOrTagName $BranchOrTagName -RuntimeIdentifier $RuntimeIdentifier
     $zipPath = Join-Path -Path $outputLocation -ChildPath $FileName
 
     Compress-Archive -Path $OutputDir/* -DestinationPath $zipPath
--- .azure-pipelines/powershell/ComputeVersion.ps1
+++
@@ -1,9 +0,0 @@
-Param(
-    [string]
-    $BranchOrTagName = "latest"
-)
-# Get the version
-$version = "$BranchOrTagName"
-$version = $version.TrimStart('refs/tags/v')
-# Return the version
-Write-Output $version
