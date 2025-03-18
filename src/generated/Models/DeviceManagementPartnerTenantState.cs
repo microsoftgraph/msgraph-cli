94d12b5c7973ff763e201bfee8a982eed68f89d7
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-03-18 10:47:38 +0000

    feat(generation): update request builders and models
    Update generated files with build 182989

--- src/generated/Models/DeviceManagementPartnerTenantState.cs
+++ src/generated/Models/DeviceManagementPartnerTenantState.cs
@@ -4,6 +4,7 @@ using System;
 namespace ApiSdk.Models
 {
     /// <summary>Partner state of this tenant.</summary>
+    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public enum DeviceManagementPartnerTenantState
     {
         /// <summary>Partner state is unknown.</summary>
