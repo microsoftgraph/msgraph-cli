4756bcd4bf8257da35e485f3f2676cb37a4af98a
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-05-27 10:34:14 +0000

    feat(generation): update request builders and models
    Update generated files with build 188742

--- src/generated/Models/VisibilitySetting.cs
+++ src/generated/Models/VisibilitySetting.cs
@@ -4,6 +4,7 @@ using System;
 namespace ApiSdk.Models
 {
     /// <summary>Generic visibility state.</summary>
+    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public enum VisibilitySetting
     {
         /// <summary>Not configured.</summary>
