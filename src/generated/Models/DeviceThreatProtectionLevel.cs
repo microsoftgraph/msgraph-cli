e11b8a589c8360d1166d41007a52ce8f300eb5f6
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-06-03 10:39:24 +0000

    feat(generation): update request builders and models
    Update generated files with build 189193

--- src/generated/Models/DeviceThreatProtectionLevel.cs
+++ src/generated/Models/DeviceThreatProtectionLevel.cs
@@ -4,6 +4,7 @@ using System;
 namespace ApiSdk.Models
 {
     /// <summary>Device threat protection levels for the Device Threat Protection API.</summary>
+    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public enum DeviceThreatProtectionLevel
     {
         /// <summary>Default Value. Do not use.</summary>
